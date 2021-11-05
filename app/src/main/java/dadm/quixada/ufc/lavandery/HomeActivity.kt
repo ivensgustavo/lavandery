4aa8f894b132b8a8312daa59f05ef4c54052f594
ivensgustavo <gustavo_ivens@alu.ufc.br>
2021-11-05 20:56:23 -0300

    Create bottom navigation menu

---
+++ app/src/main/java/dadm/quixada/ufc/lavandery/HomeActivity.kt
@@ -0,0 +1,44 @@
+package dadm.quixada.ufc.lavandery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import dadm.quixada.ufc.lavandery.fragments.HomeFragment
+import dadm.quixada.ufc.lavandery.fragments.MyRequestsFragment
+import dadm.quixada.ufc.lavandery.fragments.SettingsFragment
+
+class HomeActivity : AppCompatActivity() {
+
+    private val homeFragment: Fragment = HomeFragment()
+    private val myRequestFragment: Fragment = MyRequestsFragment()
+    private val settingsFragment: Fragment = SettingsFragment()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        changeCurrentFragment(homeFragment)
+        setBottomNavigationActions()
+    }
+
+    private fun setBottomNavigationActions() {
+        val bottomNavigationMenu: BottomNavigationView = findViewById(R.id.bottom_navigation_menu)
+        bottomNavigationMenu.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.home_nav_button -> changeCurrentFragment(homeFragment)
+                R.id.my_requests_nav_button -> changeCurrentFragment(myRequestFragment)
+                else -> changeCurrentFragment(settingsFragment)
+            }
+            true
+        }
+
+    }
+
+    private fun changeCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.container_screens, fragment)
+            commit()
+        }
+    }
+}
--- app/src/main/java/dadm/quixada/ufc/lavandery/MainActivity.kt
+++ app/src/main/java/dadm/quixada/ufc/lavandery/MainActivity.kt
@@ -1,11 +1,19 @@
 package dadm.quixada.ufc.lavandery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnEnter: Button = findViewById(R.id.enter_login_button)
+        btnEnter.setOnClickListener {
+            val homeIntent = Intent(this, HomeActivity::class.java)
+            startActivity(homeIntent)
+        }
     }
 }
