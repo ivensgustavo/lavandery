c4802a0a4c0da5f63ee11cd835e1839530831823
ivensgustavo <gustavo_ivens@alu.ufc.br>
2022-01-02 10:03:53 -0300

    Add data validation to login

--- app/src/main/java/dadm/quixada/ufc/lavandery/LoginActivity.kt
+++ app/src/main/java/dadm/quixada/ufc/lavandery/LoginActivity.kt
@@ -3,7 +3,6 @@ package dadm.quixada.ufc.lavandery
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
