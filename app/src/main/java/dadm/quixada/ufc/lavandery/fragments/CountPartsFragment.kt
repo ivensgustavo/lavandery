e7cff6b69c4f6d04ab384928887650343374dcd5
ivensgustavo <gustavo_ivens@alu.ufc.br>
2022-01-06 13:32:54 -0300

    Create frontend fragment counter parts

---
+++ app/src/main/java/dadm/quixada/ufc/lavandery/fragments/CountPartsFragment.kt
@@ -0,0 +1,22 @@
+package dadm.quixada.ufc.lavandery.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dadm.quixada.ufc.lavandery.R
+
+
+class CountPartsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_count_parts, container, false)
+    }
+
+
+}
