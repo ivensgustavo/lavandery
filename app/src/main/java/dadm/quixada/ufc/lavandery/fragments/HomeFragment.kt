02651d18c696fbc9953febd2b628b9bf0a521067
ivensgustavo <gustavo_ivens@alu.ufc.br>
2022-01-06 23:58:44 -0300

    Create new order fragment

--- app/src/main/java/dadm/quixada/ufc/lavandery/fragments/HomeFragment.kt
+++ app/src/main/java/dadm/quixada/ufc/lavandery/fragments/HomeFragment.kt
@@ -1,14 +1,13 @@
 package dadm.quixada.ufc.lavandery.fragments
 
-import android.content.Intent
+
 import android.os.Bundle
+import android.text.TextUtils.replace
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import com.google.firebase.auth.FirebaseAuth
-import dadm.quixada.ufc.lavandery.LoginActivity
 import dadm.quixada.ufc.lavandery.R
 
 class HomeFragment : Fragment() {
@@ -19,5 +18,19 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnNewOrder: Button = view.findViewById(R.id.btn_new_order)
+        btnNewOrder.setOnClickListener {
+            val fragmentManager = requireActivity().supportFragmentManager
+            fragmentManager.beginTransaction().apply {
+                replace(R.id.container_screens, NewOrderFragment())
+                addToBackStack("New order screen")
+                commit()
+            }
+        }
     }
 }
---
+++ app/src/main/java/dadm/quixada/ufc/lavandery/fragments/NewOrderFragment.kt
@@ -0,0 +1,31 @@
+package dadm.quixada.ufc.lavandery.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import dadm.quixada.ufc.lavandery.R
+
+
+class NewOrderFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_order, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bottomNavigationMenu =
+            requireActivity().findViewById<BottomNavigationView>(R.id.bottom_navigation_menu)
+
+        bottomNavigationMenu.visibility = View.GONE
+    }
+}
