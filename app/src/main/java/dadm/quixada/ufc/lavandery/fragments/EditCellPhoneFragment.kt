941eb1dfdfb90f5d63897c0f8562495aa183e8a8
ivensgustavo <gustavo_ivens@alu.ufc.br>
2022-01-09 15:51:22 -0300

    Save cell phone changes to db

--- app/src/main/java/dadm/quixada/ufc/lavandery/fragments/EditCellPhoneFragment.kt
+++ app/src/main/java/dadm/quixada/ufc/lavandery/fragments/EditCellPhoneFragment.kt
@@ -7,7 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import dadm.quixada.ufc.lavandery.R
 
 
@@ -16,6 +21,7 @@ class EditCellPhoneFragment : Fragment() {
     private lateinit var cellPhoneEditText: TextInputEditText
     private lateinit var saveCellPhoneButton: Button
     private lateinit var backButton: ImageView
+    private lateinit var  mAuth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +34,7 @@ class EditCellPhoneFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        mAuth = FirebaseAuth.getInstance()
         initializeViews(view)
         fillFields()
     }
@@ -36,6 +43,10 @@ class EditCellPhoneFragment : Fragment() {
         cellPhoneEditText = view.findViewById(R.id.cell_phone_input_text_edit)
         saveCellPhoneButton = view.findViewById(R.id.save_cell_phone_button)
         backButton = view.findViewById(R.id.back_button)
+
+        saveCellPhoneButton.setOnClickListener {
+            saveCellPhone()
+        }
 
         backButton.setOnClickListener {
             backToPreviousScreen()
@@ -49,6 +60,37 @@ class EditCellPhoneFragment : Fragment() {
 
     private fun backToPreviousScreen() {
         requireActivity().supportFragmentManager.popBackStack()
+    }
+
+    private fun saveCellPhone(){
+        val db = Firebase.firestore
+        val currentUserId = mAuth.currentUser!!.uid
+
+        db.collection("users").document(currentUserId)
+            .update("telephone", cellPhoneEditText.text.toString())
+            .addOnCompleteListener{ task ->
+                if(task.isSuccessful){
+                    showConfirmationMessage()
+                } else {
+                    Toast.makeText(
+                        requireActivity(),
+                        "Ocorreu um ao salvar o número de celular",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    private fun showConfirmationMessage() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle("Dados alterados")
+            .setMessage(
+                "O número de celular foi alterado com sucesso"
+            )
+            .setPositiveButton("Ok") { dialog, which ->
+                backToPreviousScreen()
+            }
+            .show()
     }
 
 
