10dcd302f65583c0925db2dfcae2e4183fec2a6e
ivensgustavo <gustavo_ivens@alu.ufc.br>
2022-01-20 21:43:16 -0300

    Save profile image in firebase storage

--- app/src/main/java/dadm/quixada/ufc/lavandery/fragments/AccountSettingsFragment.kt
+++ app/src/main/java/dadm/quixada/ufc/lavandery/fragments/AccountSettingsFragment.kt
@@ -1,19 +1,29 @@
 package dadm.quixada.ufc.lavandery.fragments
 
+import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ListView
-import android.widget.TextView
+import android.widget.*
+import com.bumptech.glide.Glide
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import dadm.quixada.ufc.lavandery.LoginActivity
 import dadm.quixada.ufc.lavandery.R
 import dadm.quixada.ufc.lavandery.adapters.AccountSettingAdapter
 import dadm.quixada.ufc.lavandery.internalModels.AccountSetting
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class AccountSettingsFragment : Fragment() {
@@ -25,6 +35,11 @@ class AccountSettingsFragment : Fragment() {
     private var userSurname: String = ""
     private var userEmail: String = ""
     private var userCellPhone: String = ""
+    private var profileImageUrl: String = ""
+    private lateinit var profileImageView: ImageView
+    private lateinit var btnChangeImageProfile: Button
+    private lateinit var selectedImageUri: Uri
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -51,13 +66,16 @@ class AccountSettingsFragment : Fragment() {
                     this.userSurname = document.data!!["surname"].toString()
                     this.userCellPhone = document.data!!["telephone"].toString()
                     this.userEmail = mAuth.currentUser!!.email.toString()
+                    this.profileImageUrl = document.data!!["profile_img_url"].toString()
                     this.initializeViews(view)
-                } else {
-                    Log.d("AccountSettings", "No such document")
                 }
             }
-            .addOnFailureListener { exception ->
-                Log.d("AccountSettings", "get failed with ", exception)
+            .addOnFailureListener { _ ->
+                Toast.makeText(
+                    context,
+                    "Erro ao buscar as configurações da conta",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
     }
 
@@ -67,6 +85,35 @@ class AccountSettingsFragment : Fragment() {
         accountSettingsListView.adapter = AccountSettingAdapter(requireActivity(), accountSettingsList)
 
         exitButton = view.findViewById(R.id.btn_sign_out_app)
+        exitButton.setOnClickListener { exitTheApp() }
+
+
+        profileImageView = view.findViewById(R.id.profile_image_view)
+        btnChangeImageProfile = view.findViewById(R.id.button_change_image_profile)
+        btnChangeImageProfile.setOnClickListener { selectImageProfile() }
+
+        Glide.with(requireContext())
+            .load(profileImageUrl)
+            .into(profileImageView)
+
+    }
+
+    private fun exitTheApp(){
+        mAuth.signOut()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+
+        googleSignInClient.signOut()
+
+        val loginIntent = Intent(context, LoginActivity::class.java)
+        startActivity(loginIntent)
+
+        requireActivity().finish()
     }
 
     private fun populateAccountSettingsList(): ArrayList<AccountSetting> {
@@ -79,4 +126,69 @@ class AccountSettingsFragment : Fragment() {
         return list
     }
 
+    private fun selectImageProfile(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, R.integer.REQUEST_IMAGE_PROFILE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        updateProfileImage(data)
+
+    }
+
+    private fun updateProfileImage(data: Intent?){
+        val imageUri = data?.data
+        selectedImageUri = imageUri!!
+        val bitmap = MediaStore.Images.Media.getBitmap(requireActivity().contentResolver, imageUri)
+        profileImageView.setImageDrawable(BitmapDrawable(bitmap))
+        uploadProfileToStorage()
+    }
+
+    private fun uploadProfileToStorage(){
+        val userId = mAuth.currentUser!!.uid
+        val filename = userId+"_profile_img"
+        var storageRef = FirebaseStorage.getInstance().getReference("/images/" + filename)
+        storageRef.putFile(selectedImageUri)
+            .addOnSuccessListener {
+                storageRef.downloadUrl.addOnSuccessListener { uri ->
+                    saveProfileImageUrlInDB(uri.toString())
+                }
+
+            }
+            .addOnFailureListener {
+                Toast.makeText(
+                    context,
+                    "Erro ao salvar imagem no Storage",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+    }
+
+    private fun saveProfileImageUrlInDB(url: String){
+        val db = Firebase.firestore
+        val userID = FirebaseAuth.getInstance().currentUser!!.uid
+
+        db.collection("users").document(userID)
+            .update("profile_img_url", url)
+            .addOnCompleteListener { task ->
+                if(task.isSuccessful){
+                    Toast.makeText(
+                        context,
+                        "Imagem salva com sucesso",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                else {
+                    Toast.makeText(
+                        context,
+                        "Erro ao salvar referencia da imagem",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+    }
 }
