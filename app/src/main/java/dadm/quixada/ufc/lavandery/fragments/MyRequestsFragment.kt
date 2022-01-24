93044af572199a905e0fe2388253397c454b5335
ivensgustavo <gustavo_ivens@alu.ufc.br>
2022-01-24 14:17:24 -0300

    Initialize the orders screen with the orders coming from db

--- app/src/main/java/dadm/quixada/ufc/lavandery/fragments/MyRequestsFragment.kt
+++ app/src/main/java/dadm/quixada/ufc/lavandery/fragments/MyRequestsFragment.kt
@@ -1,82 +1,55 @@
 package dadm.quixada.ufc.lavandery.fragments
 
-import android.os.Build
+
 import android.os.Bundle
-import android.text.TextUtils.replace
-import android.util.Log
+
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ListView
-import androidx.annotation.RequiresApi
-import dadm.quixada.ufc.lavandery.HomeActivity
+import android.widget.Toast
+
+import com.google.firebase.Timestamp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 import dadm.quixada.ufc.lavandery.R
 import dadm.quixada.ufc.lavandery.adapters.OrdersAdapter
+
 import dadm.quixada.ufc.lavandery.internalModels.Order
-import java.time.LocalDate
-import java.util.*
+
 import kotlin.collections.ArrayList
 
+
 class MyRequestsFragment : Fragment() {
+
+    private var orderList: ArrayList<Order> = ArrayList()
+    private lateinit var ordersAdapter: OrdersAdapter
+    private lateinit var orderListView: ListView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_my_requests, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val orderList: ArrayList<Order> = ArrayList()
+        getOrdersFromDB(view)
+    }
 
-        orderList.add(
-            Order(
-                "SGG47HHDG6",
-                "GHDN78DH4G", 30,
-                70.0.toFloat(),
-                Date(2021, 12, 17),
-                Date(2021, 12, 18),
-                Date(2021, 12, 19),
-                "Lavando"
-            )
-        )
+    private fun initializeViews(view: View) {
+        this.ordersAdapter = OrdersAdapter(this.requireActivity(), this.orderList)
+        this.orderListView = view.findViewById(R.id.order_list_view)
 
-        orderList.add(
-            Order(
-                "SGG47HGDG6",
-                "GH7N78DH4G",
-                30,
-                70.0.toFloat(),
-                Date(2021, 12, 13),
-                Date(2021, 12, 14),
-                Date(2021, 12, 15),
-                "Entregue"
-            )
-        )
+        this.orderListView.adapter = ordersAdapter
 
-        orderList.add(
-            Order(
-                "SGG47HGDG6",
-                "GH7N78DH4G",
-                30,
-                70.0.toFloat(),
-                Date(2021, 12, 8),
-                Date(2021, 12, 9),
-                Date(2021, 12, 10),
-                "Agendado"
-            )
-        )
-
-        val ordersAdapter = OrdersAdapter(this.requireActivity(), orderList)
-        val orderListView: ListView = view.findViewById(R.id.order_list_view)
-
-        orderListView.adapter = ordersAdapter
-
-        orderListView.setOnItemClickListener { _, _, _, _ ->
+        this.orderListView.setOnItemClickListener { _, _, _, _ ->
 
             requireActivity().supportFragmentManager.beginTransaction().apply {
                 replace(R.id.container_screens, OrderDetails())
@@ -85,4 +58,41 @@ class MyRequestsFragment : Fragment() {
             }
         }
     }
+
+    private fun getOrdersFromDB(view: View) {
+        val mAuth = FirebaseAuth.getInstance()
+        val userId = mAuth.currentUser!!.uid
+        val db = Firebase.firestore
+
+        db.collection("orders").whereEqualTo("consumerId", userId)
+            .get()
+            .addOnSuccessListener { documents ->
+                this.orderList.clear()
+                for(document in documents){
+                    val order = Order(
+                        document.data["consumerId"].toString(),
+                        document.data["providerId"].toString(),
+                        document.data["totalItems"].toString().toInt(),
+                        document.data["value"].toString().toFloat(),
+                        (document.data["creationDate"] as Timestamp).toDate(),
+                        (document.data["collectionDate"] as Timestamp).toDate(),
+                        (document.data["deliveryDate"] as Timestamp).toDate(),
+                        document.data["status"].toString()
+                    )
+
+                    this.orderList.add(order)
+                }
+
+                initializeViews(view)
+
+            }
+            .addOnFailureListener {
+                Toast.makeText(
+                    context,
+                    "Ocorreu um erro ao buscar seus pedidos",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+    }
+
 }
